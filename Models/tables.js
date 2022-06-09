01d0468a8c615841fd1b54d623a43f3dde124295
James Green <jamesgreen.311@gmail.com>
2022-06-09 10:25:22 -0400

    Model definitions

---
+++ Models/tables.js
@@ -0,0 +1,10 @@
+var tabs = [
+    "Application Detail",
+    "Art Memberships",
+    "Education Background",
+    "Exhibitions",
+    "Membership Interest",
+    "Sites",
+    "Config",
+    "Legend"
+]
