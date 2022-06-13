c7ea14a94de809dd0f3b6b588e2d46e3512dc9d6
James Green <jamesgreen.311@gmail.com>
2022-06-13 06:19:42 -0400

    Get Sheet Test Cases

--- TestCases/testGetSheet.js
+++ TestCases/testGetSheet.js
@@ -1,4 +1,42 @@
-function testGetSheet() {
-    let sh = getSheet("Application Detail")
-    console.log(!!sh)
+// test for an existing sheet 
+// expected result: true = found
+function testGetSheet1(verbose) {
+    let expected = true
+    let t = "1"
+    let sh
+
+    try {
+        sh = getSheet("Application Detail")
+    } catch (error) {
+        console.log(error)
+    }
+
+    if (verbose) {
+        console.log("test %s for Get Sheet: %s", t, sh.getName())
+    }
+    return console.log((!!sh)===expected?"test %s pass":"test %s fail", t)
+}
+
+// test for a non existent sheet
+// expected result: false = not found
+function testGetSheet2(verbose) {
+    let expected = false
+    let t = "2"
+    let sh
+
+    try {
+        sh = getSheet("Bad Sheet")
+    } catch (error) {
+        console.log(error)
+    }
+
+    if (verbose) {
+        console.log("test %s for Get Sheet: %s", t, "not found")
+    }
+    return console.log((!!sh)===expected?"test %s pass":"test %s fail", t)
+}
+
+function testGetSheetRunAll() {
+    testGetSheet1(true)
+    testGetSheet2(true)
 }
