4c4a3cc45456ab49e41e2cfaef48755ff916d4c7
james green <jamesgreen.311@gmail.com>
2022-06-17 11:49:28 -0400

    Fetching Chairperson Name & Email

--- scripts/endpoint.js
+++ scripts/endpoint.js
@@ -1,1 +1,3 @@
 var endPoint = "https://script.google.com/macros/s/AKfycbxI8Vug0xNskRYeYC-SI8O_kdVKwwYWIxo-yTkJ_lX2Fn_a7KVYvZH63lX7Oo-vD7w8/exec"
+// bad end point for testing only
+var badEndPoint = "https://script.google.com/macros/s/AKfycbxUOibrylxjIrU8R2DCT0FjST911GFnpJpAVGKQOpkG/dev"
