70c5969aef544930bf9b9f1eeb627459c4ce73c7
james green <jamesgreen.311@gmail.com>
2022-11-25 15:50:17 -0500

    Payment Page, Board Members

--- src/Models/appsettings.js
+++ src/Models/appsettings.js
@@ -14,7 +14,7 @@ function getSettingsTables() {
         notificationdocid: "i2"
       },
     }
-  };
+  }
 }
 
 /**
---
+++ src/Models/boardmembers.js
@@ -0,0 +1,39 @@
+function testGetMemberByPosition() {
+    console.log(getMemberByPosition("President"))
+    console.log(getMemberByPosition("Treasurer"))
+    console.log(getMemberByPosition("Membership Chairperson"))
+}
+
+function getBoardMembersTable() {
+    return {
+      boardmembers: {
+        name: "Board Members",
+        type: "standard",
+        headers: 1,
+        schema: {
+          position: "a2:a", // list of board officer titles
+          email: "b", // position will give the row number for officers email, etc
+          firstName: "c",
+          lastName: "d"
+        },
+      }
+    }
+  }
+
+function getMemberByPosition(position) {
+    const t = getBoardMembersTable()
+    const schema = t.boardmembers.schema
+    const bm = connect(MASTER_ID).getSheetByName(t.boardmembers.name)
+    const positionList = bm.getRange(schema.position + bm.getLastRow()).getDisplayValues()
+    const rows = [].concat(...positionList)
+    let row = rows.indexOf(position) // zero or greater is found
+
+    let member = {}
+    if (row>=0) {
+        row += (1 + t.boardmembers.headers) // data starts at row 1 plus number of header rows
+        member.email = bm.getRange(`${schema.email}${row}`).getDisplayValue()
+        member.firstName = bm.getRange(`${schema.firstName}${row}`).getDisplayValue()
+        member.lastName = bm.getRange(`${schema.lastName}${row}`).getDisplayValue()
+    }
+    return member
+}
